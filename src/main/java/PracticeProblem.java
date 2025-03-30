85b0de2c27f698691c0146227ad824acdf7b4f97
arvin-deng-ics4u1-03-25 <arvin.deng@student.tdsb.on.ca>
2025-03-30 15:55:24 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,40 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
-
+	public static int find(int[] array, int number) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == number) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
-	
+	public static int findLast(String[] array, String string) {
+		for (int i = array.length - 1; i >= 0; i--) {
+			if (array[i].equals(string)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
+	public static int findSecond(char[] array, char character) {
+		int count = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == character) {
+				count++;
+				if (count == 2) {
+					return i;
+				}
+			}
+		}
+		if (count == 1) {
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] == character) {
+					return i;
+				}
+			}
+		}
+		return -1;
+	}
 }
